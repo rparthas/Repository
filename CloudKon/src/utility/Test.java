58cafab2061897c65769a1ce52b2ffe2c5fe4406
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2013-10-26 20:33:07 +0530

    Test file for creating task

---
+++ CloudKon/src/utility/Test.java
@@ -0,0 +1,24 @@
+package utility;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+public class Test {
+
+	public static void main(String args[]){
+		
+			try(BufferedWriter writer = new BufferedWriter(new FileWriter("tasks.txt"))){
+				for(int i=0;i<100;i++){
+					writer.append(1000+"");
+					writer.newLine();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally{
+				
+			}
+		
+	}
+}
