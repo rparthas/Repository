c02c6813b9d91f045305172b1c9ea01f76b818af
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2016-07-03 13:02:50 -0500

    Projects removed

--- Node/buffer.js
+++
@@ -1,3 +0,0 @@
-var buffer = new Buffer('this is a good place to start');
-var slice = buffer.slice(10, 20);
-console.log(slice);
--- Node/test.js
+++
@@ -1,5 +0,0 @@
-var checkData = require('./checkEmail');
-
-console.log("In testvalidation -->"+checkData.checkEmail("piyas.de@gmail.com"));
-
-console.log("In testvalidation -->"+checkData.checkEmail("piyas.de@gmailcom")); // Return error
