f6dff0e607c14e3639e6d12ffe338996abf3035c
Rajagopal Parthasarathi <Rajagopal Parthasarathi>
2016-10-06 19:52:01 -0500

    Scala Project Signed-off-by: Rajagopal Parthasarathi <Rajagopal Parthasarathi>

---
+++ Scala/.worksheet/src/scalasheet.scala
@@ -0,0 +1,4 @@
+object scalasheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(63); 
+  println("Welcome to the Scala worksheet");$skip(5); val res$0 = 
+	1-1;System.out.println("""res0: Int(0) = """ + $show(res$0))}
+}
