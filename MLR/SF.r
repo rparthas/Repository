89bb4ece69abae81fd19b11f1caea09763fe7166
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2017-01-16 09:58:41 +0530

    Machine Learning in R first commit

---
+++ MLR/SF.r
@@ -0,0 +1,8 @@
+els2002 <- read.csv(file = 'ELS2002-2012.csv',header = TRUE,sep = ',')
+income2011 <- els2002$income2011
+mean(income2011)
+median(income2011)
+hist(income2011,breaks = 20)
+summary(income2011)
+boxplot(income2011)
+sd(income2011)
