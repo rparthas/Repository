0167b09d17efce4624235f68e3085e0f809d091d
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2017-03-05 15:04:20 +0530

    Spark Project commit

--- GradleSpace/src/main/java/edu/learn/selenium/SeleniumKindle.java
+++ GradleSpace/src/main/java/edu/learn/selenium/SeleniumKindle.java
@@ -1,7 +1,5 @@
 package edu.learn.selenium;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,15 +10,14 @@ public class SeleniumKindle {
 
 		System.setProperty("webdriver.gecko.driver", "/home/ram/geckodriver");
 		WebDriver driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 		driver.get("https://www.ultimatix.net/");
 		
 
 		driver.findElement(By.id("USER")).sendKeys("rparthas");
-		driver.findElement(By.id("PASSWORD")).sendKeys("Family17!");
-		driver.findElement(By.id("login_button")).click();
-		Thread.sleep(2000);
+		driver.findElement(By.id("PASSWORD")).sendKeys("Talent17!");
+		driver.findElement(By.id("login_button")).submit();
 		
+		Thread.sleep(3000);
 		driver.get("https://www.ultimatix.net/ultimatixPortalWeb/portlets/applications/redirect.jsp?PARAM=463");
 		driver.switchTo().alert().accept();
 
