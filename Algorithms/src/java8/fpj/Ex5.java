942720b3737b95e5679b0d91e7ed857d1806bbf0
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2017-06-10 20:54:30 +0530

    FPJ chapter 5 and 6

--- Algorithms/src/java8/fpj/Ex5.java
+++ Algorithms/src/java8/fpj/Ex5.java
@@ -1,14 +1,17 @@
 package java8.fpj;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class Ex5 {
 
 	public static void main(String[] args) throws Throwable {
+		FileWriterEAM.use("output.txt", writer -> writer.writeStuff("cool stuff"));
 		RodCutter rodCutter = new RodCutter();
 		rodCutter.setPrices(null);
-		Throwable ex=assertThrows(RodCutterException.class, () -> rodCutter.maxProfit(1));
-		if(ex!=null)
+		Throwable ex = assertThrows(RodCutterException.class, () -> rodCutter.maxProfit(1));
+		if (ex != null)
 			throw ex;
 
 	}
@@ -47,4 +50,41 @@ class RodCutterException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 3226261000008985293L;
 
+}
+
+class FileWriterEAM {
+
+	private final FileWriter writer;
+
+	private FileWriterEAM(final String fileName) throws IOException {
+		writer = new FileWriter(fileName);
+	}
+
+	private void close() throws IOException {
+		System.out.println("close called automatically...");
+		writer.close();
+
+	}
+
+	public void writeStuff(final String message) throws IOException {
+		writer.write(message);
+	}
+
+	public static void use(final String fileName, final UseInstance<FileWriterEAM, IOException> block)
+			throws IOException {
+
+		final FileWriterEAM writerEAM = new FileWriterEAM(fileName);
+		try {
+			block.accept(writerEAM);
+		} finally {
+			writerEAM.close();
+		}
+
+	}
+
+}
+
+@FunctionalInterface
+interface UseInstance<T, X extends Throwable> {
+	void accept(T instance) throws X;
 }
