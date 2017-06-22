5c230a97c7bc7166db9ad0f3436f6e0d5060dd84
Rajagopal Parthasarathi <rajagopal.sarathi@gmail.com>
2017-06-22 11:15:49 +0530

    Amazon Warehouse Scalability problem

--- Algorithms/src/learn/techgig/WorkQueue.java
+++ Algorithms/src/learn/techgig/WorkQueue.java
@@ -1,33 +1,52 @@
 package learn.techgig;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.stream.IntStream;
 
 public class WorkQueue {
 
 	static Map<String, Queue<Task>> workQueue = new ConcurrentHashMap<String, Queue<Task>>();
-	static Map<String, String> workTracker = Collections.synchronizedMap(new HashMap<String, String>());
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		ExecutorService threadPoolExecutor = new ThreadPoolExecutor(20, 100, 5000, TimeUnit.MILLISECONDS,
-				new LinkedBlockingQueue<Runnable>());
+		int skillCount = 10;
+		int workerCountPerSkill = 10;
+		int taskCount = 1000000;
+		ExecutorService workerPool = Executors.newFixedThreadPool(skillCount * workerCountPerSkill);
+		Random randomSkillGenerator = new Random();
+		Random randomPriorityGenerator = new Random();
 
-		for (int i = 1; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
+		String[] skills = new String[skillCount];
+		IntStream.rangeClosed(1, skillCount).forEach(counter -> skills[counter - 1] = "S" + counter);
+
+		for (int j = 0; j < skillCount; j++) {
+			for (int i = 1; i < workerCountPerSkill; i++) {
 				WorkQueue.Worker worker = new WorkQueue.Worker();
-				worker.skill = "S" + i;
-				threadPoolExecutor.execute(worker);
+				worker.skill = "S" + j;
+				workerPool.execute(worker);
 			}
 		}
+
+		IntStream.rangeClosed(1, taskCount).forEach(counter -> {
+			int randIndex = randomSkillGenerator.nextInt(skills.length);
+			Task task = new Task();
+			task.skill = skills[randIndex];
+			task.priority = randomPriorityGenerator.nextInt(200);
+			Queue<Task> taskQueue = workQueue.get(task.skill);
+			if (taskQueue == null) {
+				taskQueue = new PriorityBlockingQueue<>();
+				workQueue.put(task.skill, taskQueue);
+			}
+			taskQueue.add(task);
+		});
 
 	}
 
@@ -42,9 +61,9 @@ public class WorkQueue {
 
 		@Override
 		public int compareTo(Task task) {
-			if (priority > task.priority)
+			if (priority < task.priority)
 				return 1;
-			else if (priority < task.priority)
+			else if (priority > task.priority)
 				return -1;
 			return 0;
 		}
@@ -62,13 +81,12 @@ public class WorkQueue {
 		public void run() {
 			while (true) {
 				Queue<Task> queue = workQueue.get(skill);
-				if (!queue.isEmpty()) {
-					Task task = queue.peek();
-					if (!workTracker.containsKey(task.id)) {
-						workTracker.put(task.id, id);
-						// executes
+				if (queue != null && !queue.isEmpty()) {
+					Task task = queue.poll();
+					if (task != null) {
+						System.out.println("Worker[" + id + "] executing [" + task.id + "]");
 					}
-					// does not execute in else block
+
 				}
 			}
 
