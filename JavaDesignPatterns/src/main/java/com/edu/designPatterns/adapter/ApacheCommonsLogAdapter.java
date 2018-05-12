14f9d721e3b7f6cd7f6a980e7066140751fda37e
Rajagopal <rajagopal.sarathi@gmail.com [commit]>
2018-05-12 14:56:23 +0530

    Java Design Patterns

--- JavaDesignPatterns/src/main/java/com/edu/designPatterns/adapter/ApacheCommonsLogAdapter.java
+++ JavaDesignPatterns/src/main/java/com/edu/designPatterns/adapter/ApacheCommonsLogAdapter.java
@@ -6,15 +6,15 @@ import org.apache.commons.logging.impl.Log4JLogger;
 public class ApacheCommonsLogAdapter extends Logger {
     private Log logger = new Log4JLogger("MyLogger");
 
-    public void trace(String str){
+    public void trace(String str) {
         logger.trace(str);
     }
 
-    public void debug(String str){
+    public void debug(String str) {
         logger.debug(str);
     }
 
-    public void log(String str){
+    public void log(String str) {
         logger.info(str);
     }
 }
