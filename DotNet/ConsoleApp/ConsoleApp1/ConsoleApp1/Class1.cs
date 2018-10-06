2a9ce1153a83febd7785923ecd71170daf4ad97e
Rajagopal <psraja@thoughtworks.com [commit]>
2018-10-06 11:37:13 +0530

    ConsoleApp and WebApp for dotnet

---
+++ DotNet/ConsoleApp/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        private int _IntProperty;
+
+        public int IntProperty
+        {
+            get => this._IntProperty > 0 ? this._IntProperty : -1;
+
+            set => this._IntProperty = value + 1;
+
+        }
+        public string myProperty { get; set; }
+
+        public void method1(int parameter1, string parameter2)
+        {
+            Console.WriteLine("Parameter1:{0} ,Parameter 2: {1}", parameter1, parameter2);
+        }
+    }
+}
