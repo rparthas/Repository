aa32f6c4c64d83e210ea8cc2e60d6178df0d941f
Rajagopal <psraja@thoughtworks.com [commit]>
2018-10-09 08:48:02 +0530

    File Stream handling in C#

---
+++ DotNet/ConsoleApp/File/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Z:\\git\\repository\\DotNet\\ConsoleApp\\File\\";
+            FileSync(path);
+            FileAsync(path);
+
+        }
+
+        private static void FileSync(string path)
+        {
+            using (StreamReader reader = new StreamReader($"{path}/random.txt"))
+            {
+                String contents = reader.ReadToEnd();
+                using (StreamWriter writer = new StreamWriter($"{path}random-copy.txt"))
+                {
+                    writer.WriteLine(contents);
+                }
+            }
+        }
+
+        private static async void FileAsync(string path)
+        {
+            using (StreamReader reader = new StreamReader($"{path}/random.txt"))
+            {
+                String contents = await reader.ReadLineAsync();
+                using (StreamWriter writer = new StreamWriter($"{path}random-copy.txt"))
+                {
+                    while (contents != null)
+                    {
+                        await writer.WriteAsync(contents);
+                        contents = await reader.ReadLineAsync();
+                    }
+                }         
+            }
+        }
+    }
+}
