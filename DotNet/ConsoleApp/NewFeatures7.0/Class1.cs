cbcdcd9100b0ccca01fc3649db33e3c519c6e300
Rajagopal <psraja@thoughtworks.com [commit]>
2018-10-10 13:15:43 +0530

    New features C# 7.0

---
+++ DotNet/ConsoleApp/NewFeatures7.0/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewFeatures7._0
+{
+    class Person
+    {
+        public string name { get; private set; }
+
+        private int _age;
+
+        public int Age { get => _age; private set => _age = value; }
+
+
+        public Person(string name,int age)
+        {
+            this.name = name ?? throw new ArgumentNullException("Name cannot be null");
+            _age = age;
+        }
+
+        public ref int GetAge() => ref _age;
+
+        public override string ToString() => $"Name:{name} Age:{_age}";
+    }
+
+    class Student : Person
+    {
+        public Student(string name, int age) :base(name, age)
+        {
+
+        }
+    }
+
+    class Employee : Person
+    {
+        public Employee(string name, int age) : base(name, age)
+        {
+
+        }
+    }
+}
---
+++ DotNet/ConsoleApp/NewFeatures7.0/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NewFeatures7._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TupleMethod();
+            //PatternMatchingMethod();
+            //RefMethod();
+            //ThrowExceptionMethod();
+            //GetAsyncTask();
+
+            int binary = 0b1000_0100;
+            Console.WriteLine(binary);
+
+            Console.ReadKey();
+        }
+
+        private static async ValueTask<bool> GetAsyncTask()
+        {
+            await Task.Delay(1000);
+            return false;
+        }
+
+        private static void ThrowExceptionMethod()
+        {
+            try
+            {
+                Student s = new Student(null, 2);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void RefMethod()
+        {
+            Person person = new Student("1", 12);
+            int age = person.Age;
+            add(ref age);
+
+            Console.WriteLine($"Person is {person}");
+
+            ref int refAge = ref person.GetAge();
+            add(ref refAge);
+            Console.WriteLine($"Person is {person}");
+
+            void add(ref int number)
+            {
+                number++;
+            }
+        }
+
+        private static void PatternMatchingMethod()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Student("1", 12));
+            people.Add(new Employee("2", 27));
+            people.Add(new Employee("3", 35));
+            people.Add(new Student("4", 19));
+
+            foreach (var person in people)
+            {
+                switch (person)
+                {
+                    case Student s when s.Age <= 17:
+                        Console.WriteLine($"{person.name} is school student");
+                        break;
+                    case Student s when s.Age > 18:
+                        Console.WriteLine($"{person.name} is college student");
+                        break;
+                    case Employee e when e.Age <= 30:
+                        Console.WriteLine($"{person.name} is Junior Employee");
+                        break;
+                    case Employee e when e.Age > 30:
+                        Console.WriteLine($"{person.name} is Senior Employee");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void TupleMethod()
+        {
+            var result = AddAndDivide(6, 3);
+            Console.WriteLine($"Sum is {result.sum} and division is {result.div}");
+            int sum;
+            double div;
+            (sum, div) = AddAndDivide(6, 3);
+            Console.WriteLine($"Sum is {sum} and division is {div}");
+        }
+
+        static (int sum, double div) AddAndDivide(int a, int b) => (a + b, a / b);
+    }
+}
