120471743026d12934c22413631cb6827aa33b0f
Rajagopal <psraja@thoughtworks.com [commit]>
2018-12-23 09:00:55 +0530

    folder change in filecrawler

--- Python/file_crawler.py
+++ Python/file_crawler.py
@@ -2,7 +2,7 @@ from os import walk
 import re
 
 f = []
-my_path = "E:\\Media\\Breaking Bad\\Breaking Bad Season 5"
+my_path = "."
 for (dir_path, dir_names, file_names) in walk(my_path):
     f.extend(file_names)
     break
@@ -11,9 +11,8 @@ extn = ".mkv"
 for file_name in file_names:
     if extn in file_name:
         file_name = file_name.replace(extn, "")
-        #print(file_name)
+        # print(file_name)
         # print(file_name.replace("",""))
 
 for i in range(16):
-        print(f"And(a=a[{i}],b=b[{i}],out=out[{i}]);")
-
+    print(f"And(a=a[{i}],b=b[{i}],out=out[{i}]);")
