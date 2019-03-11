64cc00fb9fe285161fbf650e2502a5da0ce7c0e5
Rajagopal <psraja@thoughtworks.com>
2019-03-11 09:02:56 +0530

    Algorithms in C#- Merge Sort Optimization

--- DotNet/ConsoleApp/Algorithms/MergeSort.cs
+++ DotNet/ConsoleApp/Algorithms/MergeSort.cs
@@ -13,39 +13,44 @@ namespace Algorithms
 
         private void MergeSorter(int[] numbers, int start, int end)
         {
-            int middle = (end + start ) / 2;
+            int middle = (end + start) / 2;
             if (start >= end)
             {
                 return;
             }
 
             MergeSorter(numbers, start, middle);
-            MergeSorter(numbers, middle+1, end);
-            Merge(numbers,start,middle,end);
+            MergeSorter(numbers, middle + 1, end);
+            Merge(numbers, start, middle, end);
         }
 
 
-        private void Merge(int[] numbers, int start,int middle, int end)
+        private void Merge(int[] numbers, int start, int middle, int end)
         {
-            var left = new int[middle - start + 2];
-            var right = new int[end - middle+1];
-            left[left.Length - 1] = int.MaxValue;
-            right[right.Length - 1] = int.MaxValue;
-            
-            for (int k = 0; k < left.Length-1; k++)
+            var left = new int[middle - start + 1];
+            var right = new int[end - middle];
+
+            for (var k = 0; k < left.Length; k++)
             {
                 left[k] = numbers[start + k];
             }
-            for (int k = 0; k < right.Length-1; k++)
+
+            for (var k = 0; k < right.Length; k++)
             {
-                right[k] = numbers[middle+1 + k];
+                right[k] = numbers[middle + 1 + k];
             }
-            int low = 0;
-            int high = 0;
-            
-            for (int k = start; k <= end; k++)
+
+            var low = 0;
+            var high = 0;
+
+            for (var k = start; k <= end; k++)
             {
-                if (left[low] <= right[high])
+                if (low >= left.Length)
+                {
+                    numbers[k] = right[high];
+                    high++;
+                }
+                else if (high >= right.Length || left[low] <= right[high])
                 {
                     numbers[k] = left[low];
                     low++;
@@ -57,6 +62,5 @@ namespace Algorithms
                 }
             }
         }
-
     }
 }
