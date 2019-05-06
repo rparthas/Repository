6b6d2a2635269de8662a4ba100e8dcc18de8b496
Rajagopal <psraja@thoughtworks.com>
2019-05-06 08:28:39 +0530

    Streaming Example with sliding window

--- Spark/src/main/scala/com/scala/spark/StreamingExample.scala
+++ Spark/src/main/scala/com/scala/spark/StreamingExample.scala
@@ -1,11 +1,26 @@
 package com.scala.spark
 
-import org.apache.spark.sql.types.{StringType, StructType, TimestampType}
-import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructType, TimestampType}
+import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 
 class StreamingExample extends SparkJob {
   override def execute(spark: SparkSession): Unit = {
 
+    //    processStream(fixedWindow(spark))
+    processStream(slidingWindow(spark))
+
+  }
+
+  private def processStream(actionCountDF: DataFrame) = {
+    val mobileConsoleSQ = actionCountDF.writeStream
+      .format("console").option("truncate", "false")
+      .outputMode("complete")
+      .start()
+
+    mobileConsoleSQ.awaitTermination()
+  }
+
+  private def fixedWindow(spark: SparkSession) = {
     import spark.sqlContext.implicits._
 
     val mobileSchema = new StructType()
@@ -14,18 +29,28 @@ class StreamingExample extends SparkJob {
       .add("ts", TimestampType, false)
 
     val mobileStreaming = spark.readStream.schema(mobileSchema)
-      .json("data/mobile")
+      .json("data/iot")
 
     val actionCountDF = mobileStreaming.groupBy(functions.window('ts, "10 minutes"),
       'action).count
 
-    val mobileConsoleSQ = actionCountDF.writeStream
-      .format("console").option("truncate", "false")
-      .outputMode("complete")
-      .start()
+    actionCountDF
+  }
 
-    mobileConsoleSQ.awaitTermination()
+  private def slidingWindow(spark: SparkSession) = {
+    import spark.sqlContext.implicits._
 
+    val iotDataSchema = new StructType().add("rack", StringType, false)
+      .add("temperature", DoubleType, false)
+      .add("ts", TimestampType, false)
+
+    val mobileStreaming = spark.readStream.schema(iotDataSchema)
+      .json("data/iot")
+
+    val iotDF = mobileStreaming.groupBy(functions.window('ts, "10 minutes",
+      "5 minutes"),'rack).agg(functions.avg('temperature).as("avg_temp"))
+
+    iotDF
   }
 
   override def getJobName(): String = "StreamingExample"
