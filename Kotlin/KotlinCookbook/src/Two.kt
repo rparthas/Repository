048e9ee6066d0c67f4db56365eb4adabf30b1863
Rajagopal <psraja@thoughtworks.com>
2019-10-15 09:18:21 +0530

    Add radix support for decimal number

--- Kotlin/KotlinCookbook/src/Two.kt
+++ Kotlin/KotlinCookbook/src/Two.kt
@@ -6,4 +6,8 @@ fun main(args: Array<String>) {
     val a: Int = 3
     val b: Long = a.toLong()
     println("value of b is $b")
+
+    (Character.MIN_RADIX..Character.MAX_RADIX).forEach { radix ->
+        println("$radix: ${36.toString(radix)}")
+    }
 }
