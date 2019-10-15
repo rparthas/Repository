0631df7e7f851c3f528d1f9281906454baefe5d3
Rajagopal <psraja@thoughtworks.com>
2019-10-15 07:39:56 +0530

    Cookbook chapter2

---
+++ Kotlin/KotlinCookbook/src/Two.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val jk = Person("JK", null, "Rowling")
+    println("Length of middle name is ${jk.middleName?.length ?: 0}")
+    println("${jk as? Person}")
+
+    val a: Int = 3
+//    val b: Long = a
+//    println("value of b is $b")
+}
