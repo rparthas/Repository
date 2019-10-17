d932dcaaf07719583d275d518a56782d1dacaa49
Rajagopal <psraja@thoughtworks.com>
2019-10-17 20:44:56 +0530

    Kotlin cookbook chapter2

--- Kotlin/cookbook/src/main/java/edu/kotlin/cook/Main.kt
+++ Kotlin/cookbook/src/main/java/edu/kotlin/cook/Main.kt
@@ -1,10 +1,26 @@
 package edu.kotlin.cook
 
 import java.text.NumberFormat
+import kotlin.math.pow
 
 fun main(args: Array<String>) {
+    println("power is ${2.pow(4)}")
+    println("power is ${2L.pow(4)}")
+    println("power is ${2.0f.pow(4)}")
+    println("power is ${2.0.pow(4)}")
+    println("power is ${2 `**` 4}")
 
 }
+
+infix fun Int.`**`(x: Int) = toDouble().pow(x).toInt()
+infix fun Long.`**`(x: Int) = toDouble().pow(x).toLong()
+infix fun Float.`**`(x: Int) = pow(x)
+infix fun Double.`**`(x: Int) = pow(x)
+
+// Pattern similar to existing functions on Float and Double
+fun Int.pow(x: Int) = `**`(x)
+
+fun Long.pow(x: Int) = `**`(x)
 
 @JvmOverloads
 fun addProduct(name: String, price: Double = 0.0, desc: String? = null) =
--- Kotlin/cookbook/src/test/java/edu/kotlin/cook/test/MainTest.kt
+++ Kotlin/cookbook/src/test/java/edu/kotlin/cook/test/MainTest.kt
@@ -1,8 +1,15 @@
 package edu.kotlin.cook.test
 
 import edu.kotlin.cook.addProduct
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.hasKey
+import org.hamcrest.Matchers.hasValue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import kotlin.math.pow
+
 
 public class MainTest {
     @Test
@@ -12,5 +19,30 @@ public class MainTest {
                 { println(addProduct("Name", 5.0)) },
                 { println(addProduct("Name")) }
         )
+    }
+
+    @Test
+    fun `raise an Int to a power`() {
+        assertThat(256, equalTo(2.toDouble().pow(8).toInt()))
+    }
+
+    @Test
+    fun `create map using infix to function`() {
+        val map = mapOf("a" to 1, "b" to 2, "c" to 2)
+        assertAll(
+                { assertThat(map, hasKey("a")) },
+                { assertThat(map, hasKey("b")) },
+                { assertThat(map, hasKey("c")) },
+                { assertThat(map, hasValue(1)) },
+                { assertThat(map, hasValue(2)) })
+    }
+
+    @Test
+    fun `destructuring a Pair`() {
+        val pair = "a" to 1
+        val (x,y) = pair
+
+        assertThat(x, `is`("a"))
+        assertThat(y, `is`(1))
     }
 }
