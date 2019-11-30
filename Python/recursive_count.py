2f6d72b3a5b76c377ee7ec558d2ed47729b2b50d
Rajagopal <psraja@thoughtworks.com>
2019-11-30 12:14:10 +0530

    Implementing recursive functions

--- Python/recursive_count.py
+++
@@ -1,7 +0,0 @@
-def recursive_count(arr, index=0):
-    if len(arr) == index:
-        return 0
-    return 1 + recursive_count(arr, index + 1)
-
-
-print(recursive_count([x + 1 for x in range(100)]))
--- Python/recursive_sum.py
+++
@@ -1,7 +0,0 @@
-def recursive_sum(arr, index=0):
-    if len(arr) == index:
-        return 0
-    return arr[index] + recursive_sum(arr, index + 1)
-
-
-print(recursive_sum([x + 1 for x in range(100)]))
