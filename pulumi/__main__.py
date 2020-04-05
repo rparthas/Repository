725b5d2f3776c95b7a02b55b178c25c4397056b4
Rajagopal Parthasarathi <psraja@thoughtworks.com>
2020-04-05 18:25:39 +0530

    S3 bucket with public endpoint

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,7 +1,27 @@
 import pulumi_aws as aws
+import pulumi
+import os
+import mimetypes
 
-bucket = aws.s3.Bucket(resource_name="pulumi-demo", bucket="pulumi-demo", tags={
-    "Owner": "pulumi-demo",
-    "Purpose": "demo",
-    "Place": "atlas"
-})
+config = pulumi.Config()
+site_dir = config.get("site")
+
+bucket = aws.s3.Bucket(resource_name="pulumi-demo",
+                       bucket="pulumi-demo",
+                       tags={
+                           "Owner": "pulumi-demo",
+                           "Purpose": "demo",
+                           "Place": "atlas"
+                       },
+                       website={"indexDocument": "index.html"})
+filepath = os.path.join(site_dir, "index.html")
+mimetype,_=mimetypes.guess_type(filepath)
+bucket_object = aws.s3.BucketObject(
+    "index.html",
+    bucket=bucket.bucket,
+    acl="public-read",
+    source=filepath,
+    content_type=mimetype)
+
+pulumi.export('bucket_name', bucket.bucket)
+pulumi.export('bucket_endpoint', bucket.website_endpoint)
